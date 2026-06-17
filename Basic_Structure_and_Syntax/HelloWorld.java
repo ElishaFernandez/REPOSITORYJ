0fe10af2dec49cc25e070d7292b9dd439a1dc98b
ElishaFernandez <fernandezelisha19@gmail.com>
2026-06-17 14:31:55 +0800

    updated

--- Basic_Structure_and_Syntax/HelloWorld.java
+++ Basic_Structure_and_Syntax/HelloWorld.java
@@ -1,19 +1,26 @@
 package Basic_Structure_and_Syntax;
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-        System.out.println("Welcome to Java programming!");
-        System.out.println("12345l67890l");
-        System.out.println("My name is Elisha, and I am learning Java.");
-        System.out.println("I live in New York.");  
-        System.out.println("I am 17 years old.");
+        // System.out.println("Hello, World!"); 
+        // System.out.println("Welcome to Java programming!");
+        // System.out.println("12345l67890l");
+        // System.out.println("My name is Elisha, and I am learning Java.");
+        // System.out.println("I live in New York.");  
+        // System.out.println("I am 17 years old.");
         System.out.println("ADDITION");
-        System.out.println("5 + 3 = " + (5 + 3));
+        System.out.print("5 + 3 = ");
+        System.out.println(5 + 3);
+
         System.out.println("SUBTRACTION");
-        System.out.println("10 - 4 = " + (10 - 4));
+        System.out.print("10 - 4 = ");
+        System.out.println(10 - 4);
+        
         System.out.println("MULTIPLICATION");
-        System.out.println("6 * 7 = " + (6 * 7));
+        System.out.print("6 * 7 = ");
+        System.out.println(6 * 7);
+        
         System.out.println("DIVISION");
-        System.out.println("20 / 5 = " + (20 / 5));
+        System.out.print("20 / 5 = ");
+        System.out.println(20 / 5);
     }
 }   
