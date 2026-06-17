1429fd7a765bbe91dd206d85553767fa1879955a
ElishaFernandez <fernandezelisha19@gmail.com>
2026-06-17 14:18:46 +0800

    updated

--- Basic_Structure_and_Syntax/HelloWorld.java
+++ Basic_Structure_and_Syntax/HelloWorld.java
@@ -3,8 +3,17 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("Welcome to Java programming!");
-        System.out.println("This is a simple Java program.");
+        System.out.println("12345l67890l");
         System.out.println("My name is Elisha, and I am learning Java.");
-
+        System.out.println("I live in New York.");  
+        System.out.println("I am 17 years old.");
+        System.out.println("ADDITION");
+        System.out.println("5 + 3 = " + (5 + 3));
+        System.out.println("SUBTRACTION");
+        System.out.println("10 - 4 = " + (10 - 4));
+        System.out.println("MULTIPLICATION");
+        System.out.println("6 * 7 = " + (6 * 7));
+        System.out.println("DIVISION");
+        System.out.println("20 / 5 = " + (20 / 5));
     }
 }   
