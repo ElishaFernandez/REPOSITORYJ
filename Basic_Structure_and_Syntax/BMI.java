26a3e64a0ff0dc8bd53651839d28a4df2dfa4f09
ElishaFernandez <fernandezelisha19@gmail.com>
2026-06-17 13:02:33 +0800

    my1stcommit

---
+++ Basic_Structure_and_Syntax/BMI.java
@@ -0,0 +1,5 @@
+package Basic_Structure_and_Syntax;
+
+public class BMI {
+    
+}
---
+++ Basic_Structure_and_Syntax/HelloWorld.java
@@ -0,0 +1,10 @@
+package Basic_Structure_and_Syntax;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+        System.out.println("Welcome to Java programming!");
+        System.out.println("This is a simple Java program.");
+        System.out.println("My name is Elisha, and I am learning Java.");
+
+    }
+}   
