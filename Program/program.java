8c6601c98e846a5e07e1061a0404d6d2b60794dc
ElishaFernandez <fernandezelisha19@gmail.com>
2026-07-27 12:19:11 +0800

    updated

--- Program/program.java
+++ Program/program.java
@@ -5,14 +5,13 @@ public class program {
     public static void main(String[] args) {
 
         System.out.println("===FOOD ORDERING SYSTEM===");
-     
 
         String food = "Burger";
-        int DiningOption = 2;
         int quantity = 3;
         int menuChoice = 2;
         int price = 300;
         int total = price * quantity;
+
 
         // SWITCH
         switch (menuChoice) {
@@ -35,18 +34,12 @@ public class program {
                 food = "Not Available";
         }
 
-        System.out.println("===Order Details===");
-        System.out.println("Food Ordered   :" + food);
-        System.out.println("Price          :" + price);
-        System.out.println("Quantity       :" + quantity);
-        System.out.println("Total Amount   :" + total);
 
-        // IF
-        System.out.println("===FREE!===");
-        if (quantity >= 3) {
-            System.out.println("Congratulations!");
-            System.out.println("You qualified for a FREE soft drink.");
-        }
+        System.out.println("===Order Details===");
+        System.out.println("Food Ordered : " + food);
+        System.out.println("Price        : " + price);
+        System.out.println("Quantity     : " + quantity);
+        System.out.println("Total Amount : " + total);
 
         // IF-ELSE
         System.out.println("===Discount===");
@@ -55,25 +48,10 @@ public class program {
             System.out.println("You received a 10% discount.");
         } else {
             System.out.println("Sorry!");
-            System.out.println("Discount is not applied.");
-            System.out.println("Minimum purchase should be ₱500.");
+            System.out.println("No discount.");
         }
 
-        // IF THEN ELSE IF
-        System.out.println("===Dining Option===");
-        if (DiningOption == 1) {
-            System.out.println("Dining option: Regular Seat");
-        } else if (DiningOption == 2) {
-            System.out.println("Dining option: Premium Seat");
-        } else if (DiningOption == 3) {
-            System.out.println("Dining option: VIP Seat");
-        } else {
-            System.out.println("Dining option: Take Out");
-        }
-
-     
         System.out.println("===Thank you for your order!===");
         System.out.println("===Enjoy your meal!===");
-      
     }
 }
