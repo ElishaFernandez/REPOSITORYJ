8071078718cd5bdeeb1365c512bd079749a1871a
ElishaFernandez <fernandezelisha19@gmail.com>
2026-09-22 18:07:51 +0800

    updated

---
+++ myMethod.java
@@ -0,0 +1,12 @@
+public class myMethod {
+
+
+static void myMethod(String name, int age) {
+    System.out.println("I'm really good at this!");
+    System.out.println("My name is " + name + " and I am " + age + " years old.");
+}
+    public static void main(String[] args) {
+        myMethod("Elisha", 17);
+
+    }
+}
